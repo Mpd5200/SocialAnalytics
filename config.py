5ec1e3e48f53e0ddf6ad27393a11bf0fc67a6af6
Michael Duffner <michaelduffner@Michaels-MacBook-Pro-4.local>
2018-07-14 15:14:34 -0400

    Social HW

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "UI8hDPaKYSTTQAotAbFPuuOg2"
+consumer_secret = "3Jacki71WHzjfx5Xkh19cqN2U9vlHHKZo6a2AdULW2sfhvFaWa"
+access_token = "714776629-hSeYrLcab8MtLH3fhYoiaRF7cYM0ruiqgNUMRXNT"
+access_token_secret = "sTK8kvSK3I28jBKRWFW0lxTTmHCtpu0LRdKiQEG6yXNd0"
